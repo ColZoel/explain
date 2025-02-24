2811b5b87a98705e6a46abc40ec90eb66922b813
ColZoel <105737771+ColZoel@users.noreply.github.com>
2025-02-23 21:39:59 -0500

    single error call

--- ado/explain/explain_modules.py
+++ ado/explain/explain_modules.py
@@ -1,20 +1,22 @@
 import sys
 import os
 import subprocess
+import json
 from builtins import print
+
 
 corpus_prompts = {
     "do": {"rewrite": "Rewrite the following Stata do-file for improved optimization and clarity:",
            "suggestfix":"Consider any lines in the following do-file that may result in an error. "
                         "Check that the syntax is correct."
-                         "Return any corrections in the form of 'current line -> corrected line'."
+                        "Return any corrections in the form of 'current line -> corrected line'."
                         "If there are no errors, return 'No errors found'."
                         "Do-file:",
            "explain": "Explain how the following do-file works:"
            },
     "code": {"rewrite": f"Rewrite the following Stata code for improved optimization and clarity:",
-        "suggestfix": f"Consider this Stata code. Check for any errors. "
-                      f"Return any corrections in the form of 'current line -> corrected line'.",
+             "suggestfix": f"Consider this Stata code. Check for any errors. "
+                           f"Return any corrections in the form of 'current line -> corrected line'.",
              "explain": f"Explain how the following Stata code works:"},
     "error": {"suggestfix": "Consider the following error message, and relevant code. What is the error? "
                             "How do I fix it?"
@@ -25,14 +27,14 @@ corpus_prompts = {
 
 
 sys_role = {
-    "short":{
+    "short": {
     "role": "system",
     "content": "You are an expert in the Stata statistical program. Your task is to answer questions about Stata code."
                "Respond as succinctly and briefly as possible. Prefer to use bullets if appropriate."
                "Do not lie. Do not make-up information. If you do not know the answer, say so."
                "You are speaking to your peers. Your response must be professional yet comfortable in tone."
     },
-    "detail":{
+    "detail": {
             "role": "system",
             "content": "You are an expert in the Stata statistical program. "
                        "Your task is to answer questions about Stata code."
@@ -69,17 +71,20 @@ def build_prompt(detail, prompt):
 
     return message
 
-def unpack(options):
-    rewrite = options["rewrite"]
-    suggestfix = options["suggestfix"]
-    detail = options["detail"]
-    verbose = options["verbose"]
-    return rewrite, suggestfix, detail, verbose
+
+def linenums(options):
+    i = options.find("lines:")
+    if i != -1:
+        lines = options[i+6:]
+    else:
+        lines = None
+    return lines
+
 
 def init():
 
     try:
-        subprocess.check_call([sys.executable, "-q","-m", "pip", "install", "aisuite"])
+        subprocess.check_call([sys.executable, "-q", "-m", "pip", "install", "aisuite"])
     except Exception as e:
         print("Error installing aisuite dependency: " + str(e))
         sys.exit(1)
@@ -105,15 +110,21 @@ def read_file(dofile, lines=None):
                 end = int(lines[1])
 
                 file_lines = file_lines[start-1:end]
-                file_lines = [f.strip('\n') for f in file_lines] # Stata is 1-indexed, adjust to 0-indexed
+                file_lines = [f.strip('\n') for f in file_lines]  # Stata is 1-indexed, adjust to 0-indexed
             else:
-                file_lines = file_lines[int(lines)-1] # line number as written in Stata
+                file_lines = file_lines[int(lines)-1]  # line number as written in Stata
 
         return "\n".join(file_lines)
+    else:
+        return ""
 
 
 def read_config(config_file):
-    import json
+
+    if config_file == "" or config_file is None:
+        print("No config file provided.")
+        sys.exit(1)
+
     try:
         with open(config_file, "r") as f:
             config = json.load(f)
@@ -134,6 +145,9 @@ def call_api(api_config, model, prompt, detail, verbose, kwargs):
         for k, v in kwargs.items():
             print(f"\n{k}: {v}")
 
+    if model is None or model == "":
+        print("model required.")
+        sys.exit(1)
 
     try:
         response = client.chat.completions.create(model=model,
@@ -147,75 +161,38 @@ def call_api(api_config, model, prompt, detail, verbose, kwargs):
     return
 
 
-def explain_do(dofile, config_file, model, options, kwargs):
+def explain_(subcmd, input, config, model, dofile, opts, kwargs):
+    configx = read_config(config)
 
-    file_lines = read_file(dofile)
-    config = read_config(config_file)
-
-    rewrite, suggestfix, detail, verbose = unpack(options)
-
-    if rewrite:
-        prompt = corpus_prompts["do"]["rewrite"] + "\n" + file_lines
-    elif suggestfix:
-        prompt = corpus_prompts["do"]["suggestfix"] + "\n" + file_lines
-    else:
-        prompt = corpus_prompts["do"]["explain"] + "\n" + file_lines
+    dofx = read_file(dofile, opts['lines'])
 
 
-    call_api(config, model, prompt, detail, verbose, kwargs)
-    return
 
-def explain_code(
-                 code=None,
-                 dofile=None,
-                 lines=None,
-                 config_file=None,
-                 model=None,
-                 options=None,
-                 kwargs=None):
 
-    rewrite, suggestfix, detail, verbose = unpack(options)
-    config = read_config(config_file)
 
-    print(f"code: {code},\n")
-    if code is None:
-        code = read_file(dofile, lines)  # or subset if lines are specified
-
-    if verbose:
-        print(f"code: {code},\n"
-              f" dofile: {dofile},\n"
-              f" lines: {lines},\n"
-              f" config: {config_file},\n"
+    if opts["verbose"]:
+        print(f" subcommand: {subcmd},\n"
+              f" input: {input},\n"
+              f" dofile path: {dofile},\n"
+              f" dofile content: {dofx},\n"
+              f" lines: {opts['lines']},\n"
+              f" config: {config},\n"
               f" model: {model},\n"
-              f" options: {options},\n"
+              f" options: {[o for o in opts]},\n"
               f" kwargs: {kwargs}")
 
+    for i in ["rewrite", "suggestfix"]:
+        if opts[i]:
+            prompt = f"{corpus_prompts[subcmd][i]}\n{input} {dofx}"
+            break
+        else:
+            prompt = f"{corpus_prompts[subcmd]['explain']}\n{input} {dofx}"
 
-    if rewrite:
-        prompt = corpus_prompts["code"]["rewrite"] + "\n" + code
-
-    elif suggestfix:
-        prompt = corpus_prompts["code"]["suggestfix"] + "\n" + code
-    else:
-        prompt = corpus_prompts["code"]["explain"] + "\n" + code
-    call_api(config, model, prompt, detail, verbose, kwargs)
+    call_api(configx, model, prompt, opts['detail'], opts['verbose'], kwargs)
     return
 
 
-def explain_error(error, config_file, model, options, kwargs):
-    config = read_config(config_file)
-
-    rewrite, suggestfix, detail, verbose = unpack(options)
-
-    if options == "suggestfix":
-        prompt = corpus_prompts["error"]["suggestfix"] + "\n" + error
-    else:
-        prompt = corpus_prompts["error"]["explain"] + "\n" + error
-
-    call_api(config, model, prompt, detail, verbose, kwargs)
-    return
-
-
+# ####### MAIN FUNCTION ########
 def main(subcommand,
          input=None,
          model=None,
@@ -224,54 +201,40 @@ def main(subcommand,
          options=None,
          max_p=None,
          top_k=None,
+         max_tokens=None,
+         temperature=None,
          frequency_penalty=None,
          presence_penalty=None,
-         stop_sequence=None,
-         max_tokens=None,
-         temperature=None):
-
-    input = input if input != "" else None
-    model = model if model != "" else None
-    config = config if config != "" else None
-    dofile = dofile if dofile != "" else None
-
+         stop_sequence=None
+         ):
 
     kwargs = {
         "max_p": max_p,
         "top_k": top_k,
+        "max_tokens": max_tokens,
+        "temperature": temperature,
         "frequency_penalty": frequency_penalty,
         "presence_penalty": presence_penalty,
-        "stop_sequence": stop_sequence,
-        "temperature": temperature,
-        "max_tokens": max_tokens
+        "stop_sequence": stop_sequence
     }
 
+    kwargs = {k: v for k, v in kwargs.items() if v is not None and v != ""}
 
-    i= options.find("lines:")
-    if i != -1:
-        lines = options[i+6:]
-    else :
-       lines = None
+    lines = linenums(options)
 
     opts = {
         "rewrite":    True if "rewrite" in options else False,
         "explain":    True if "explain" in options else False,
         "suggestfix": True if "suggestfix" in options else False,
         "detail":     True if "detail" in options else False,
+        "capture":    True if "capture" in options else False,
         "verbose":    True if "verbose" in options else False,
         "lines":      lines if "lines" in options else None
     }
 
-    kwargs = {k: v for k, v in kwargs.items() if v is not None and v!=""}
+    if subcommand in ["do", "code", "error"]:
+        explain_(subcommand, input, config, model, dofile, opts, kwargs)
 
-    if subcommand == "do":
-        explain_do(dofile, config, model, opts, kwargs)
-    elif subcommand == "code":
-
-        explain_code(input, dofile, lines, config, model, opts, kwargs)
-
-    elif subcommand == "error":
-        explain_error(input, config, model, opts, kwargs)
     elif subcommand == "init":
         try:
             import aisuite
@@ -282,8 +245,10 @@ def main(subcommand,
     else:
         print("Invalid subcommand. Please use 'do', 'code', or 'error'.")
         sys.exit(1)
+    return
 
 
+# ####### RUN MAIN FUNCTION ########
 if __name__ == "__main__":
     import argparse
     argparse = argparse.ArgumentParser()
@@ -293,15 +258,14 @@ if __name__ == "__main__":
     argparse.add_argument("config",             type=str, default=None)
     argparse.add_argument("dofile",             type=str, default=None)
     argparse.add_argument("options",            type=str, default=None)
-    argparse.add_argument("max_tokens",         type=str, default=None)
-    argparse.add_argument("temperature",        type=str, default=None)
     argparse.add_argument("max_p",              type=str, default=None)
     argparse.add_argument("top_k",              type=str, default=None)
+    argparse.add_argument("max_tokens",         type=str, default=None)
+    argparse.add_argument("temperature",        type=str, default=None)
     argparse.add_argument("frequency_penalty",  type=str, default=None)
     argparse.add_argument("presence_penalty",   type=str, default=None)
     argparse.add_argument("stop_sequence",      type=str, default=None)
     args = argparse.parse_args()
-
 
     main(args.subcommand,
          args.input,
@@ -309,10 +273,10 @@ if __name__ == "__main__":
          args.config,
          args.dofile,
          args.options,
-         args.max_tokens,
-         args.temperature,
          args.max_p,
          args.top_k,
+         args.max_tokens,
+         args.temperature,
          args.frequency_penalty,
          args.presence_penalty,
          args.stop_sequence
